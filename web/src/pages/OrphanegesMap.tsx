502cbe8eb6592d6173c725cde2ddf5dce4714fe2
Adryan1928 <adryanoliveira1928@gmail.com>
2023-04-07 11:16:16 -0300

    terminando o front-end

--- web/src/pages/OrphanegesMap.tsx
+++ web/src/pages/OrphanegesMap.tsx
@@ -4,13 +4,23 @@ import { Link } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 import { LatLngExpression } from "leaflet";
 
+import Leaflet from 'leaflet';
+
 import 'leaflet/dist/leaflet.css';
 import {FiPlus} from 'react-icons/fi';
 
 import '../styles/pages/orphanage-map.css';
 
 import mapMakerImg from '../images/map-marker.svg';
+import { LegacyOracleNamingStrategy } from "typeorm";
 const position = [-6.1029454,-38.1582802]
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMakerImg,
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2],
+})
 
 function OrphanegesMap () {
     return (
@@ -34,9 +44,9 @@ function OrphanegesMap () {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position as LatLngExpression}>
-                        <Popup>
-                            Luiz
+                    <Marker position={position as LatLngExpression} icon={mapIcon} >
+                        <Popup closeButton= {false} maxWidth={240} minWidth={240 } className="map-popup" >
+                            Orfanato
                         </Popup>
                     </Marker>
                 </MapContainer>
